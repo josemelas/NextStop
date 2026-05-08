94559fc9c555587a1593e82f523722c464a2009f
mauss <maussbrian06@gmail.com>
2026-05-08 00:14:51 -0600

    Sew quita / de urls

--- backend/NextStop/settings.py
+++ backend/NextStop/settings.py
@@ -130,7 +130,7 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # CORS y CSRF
 # ==========================
 CORS_ALLOWED_ORIGINS = [
-    "https://seal-app-u4egd.ondigitalocean.app/",
+    "https://seal-app-u4egd.ondigitalocean.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
@@ -138,7 +138,7 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://seal-app-u4egd.ondigitalocean.app/",
+    "https://seal-app-u4egd.ondigitalocean.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
