8991e88360c1e6592f2d0e59b1ed54b4ef4849d0
mauss <maussbrian06@gmail.com>
2026-05-08 02:45:11 -0600

    cambios en usuario

--- backend/NextStop/urls.py
+++ backend/NextStop/urls.py
@@ -26,5 +26,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/usuarios/', include('usuarios.urls')),
     path('api/external/', include('apis_externas.urls')),
-    path('api/vuelos/', include('vuelos.urls'))
+    path('api/vuelos/', include('vuelos.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
