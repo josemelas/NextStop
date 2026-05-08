2e0e3d8eae24d9730599e01b3b4a755656a20265
mauss <maussbrian06@gmail.com>
2026-05-08 04:08:43 -0600

    Se corrige Usuario

--- backend/usuarios/views/view_registrar.py
+++ backend/usuarios/views/view_registrar.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from ..serializers import UsuarioSerializer
-from ..models import Usuario, Verificacion, Rol, Usario_rol
+from ..models import Usuario, Verificacion, Rol, Usuario_rol
 import random, uuid
 from django.core.mail import send_mail
 from django.conf import settings
