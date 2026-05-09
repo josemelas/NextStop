fb01e0c4a18db358787c09507164505a8a39acf4
mauss <maussbrian06@gmail.com>
2026-05-08 20:17:19 -0600

    Cambios

--- backend/vuelos/models.py
+++ backend/vuelos/models.py
@@ -24,7 +24,7 @@ class Vuelo(models.Model):
                 f"{self.asientos_disponibles} - {self.creado_en}")
 
 class Reserva(models.Model):
-    Usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    Usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, db_column='id_usuario')
     Vuelo = models.ForeignKey(Vuelo, on_delete=models.CASCADE)
     codigo_confirmacion = models.CharField(max_length=20)
     fecha_transaccion = models.DateTimeField()
@@ -44,6 +44,10 @@ class Reserva(models.Model):
     )
     cantidad_pasajeros = models.IntegerField(default=1)
     asiento_asignados = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'reserva'
+        managed = False
     def __str__(self):
         return (f"{self.Usuario} - {self.Vuelo} - {self.codigo_confirmacion} - {self.fecha_transaccion} - {self.monto_total} - {self.estado_pago} - "
                 f"{self.cantidad_pasajeros} - {self.asiento_asignados}")
