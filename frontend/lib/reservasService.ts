68ae3ac926cd38ba7e30e32c38241d803ab9e8e9
José Vallejo <jose52bd@gmail.com>
2026-05-20 17:29:19 -0600

    Reservas

---
+++ frontend/lib/reservasService.ts
@@ -0,0 +1,26 @@
+// app/lib/reservasService.ts
+export const reservasService = {
+  async crearReserva(payload: {
+    vuelo_id: string;
+    usuario_id: number;
+    cantidad_pasajeros: number;
+    asientos: string;
+    monto_total: number;
+  }) {
+    try {
+      const response = await fetch('https://seal-app-u4egd.ondigitalocean.app/crearreservas/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      });
+
+      const data = await response.json();
+      return { status: response.status, data };
+    } catch (error) {
+      console.error("Error en reservasService:", error);
+      return { status: 500, data: { error: "No se pudo conectar con el servidor de NextStop." } };
+    }
+  }
+};
