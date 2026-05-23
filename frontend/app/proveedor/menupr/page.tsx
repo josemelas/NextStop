9ab3469f18d0f20e52e8a57e8743039ba4dbedfa
José Vallejo <jose52bd@gmail.com>
2026-05-23 00:10:39 -0600

    Menu proveedor

--- frontend/app/proveedor/menupr/page.tsx
+++ frontend/app/proveedor/menupr/page.tsx
@@ -80,7 +80,7 @@ export default function MenuProveedor() {
       }
     } catch (error) {
       console.error("Error al cargar el dashboard:", error);
-    } file {
+    } finally {
       setLoading(false);
     }
   };
