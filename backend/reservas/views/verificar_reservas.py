36f0e45ed23e89e63b15786f38df71d2db453678
mauss <maussbrian06@gmail.com>
2026-05-25 20:45:22 -0600

    Cambios para asientos

--- backend/reservas/views/verificar_reservas.py
+++ backend/reservas/views/verificar_reservas.py
@@ -8,15 +8,20 @@ from reservas.models import Reserva
 
 class ObtenerAsientosOcupados(APIView):
     permission_classes = [AllowAny]
-    def get(self, request, api_id_vuelo):
+    def get(self, request):
+        api_id_vuelo = request.query_params.get('vuelo_id')
+
+        if not api_id_vuelo:
+            return Response({"error": "Falta el parámetro vuelo_id"}, status=status.HTTP_400_BAD_REQUEST)
         try:
             vuelo = Vuelo.objects.get(api_id=api_id_vuelo)
             asientos_ocupados = []
-            if vuelo.asientos_ocupados:
-                lista_fantasmas = [a.strip() for a in vuelo.asientos_ocupados.split(',')]
-                asientos_ocupados.extend(lista_fantasmas)
-            reservas = Reserva.objects.filter(id_vuelo=vuelo, estado_pago='PAGADO')
 
+            if vuelo.asientos_ocupados_base:
+                lista_fantasmas = [a.strip() for a in vuelo.asientos_ocupados_base.split(',')]
+                asientos_ocupados.extend(lista_fantasmas)
+
+            reservas = Reserva.objects.filter(id_vuelo=vuelo, estado_pago='PAGADO')
             for reserva in reservas:
                 if reserva.asiento_asignado:
                     lista_reales = [a.strip() for a in reserva.asiento_asignado.split(',')]
